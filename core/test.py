75b4f5099220e68b89e3879a577baba9d89a1d39
Michel Page <michel_page38@yahoo.fr>
2015-10-03 09:55:56 +0200

    removed unused files

--- core/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import bcolz
-bz = bcolz.carray(np.array([], dtype="S500"))
-for i in range(1000):
-    bz.append(np.array(["xxx"]))
-    if len(bz) >= 2 and type(bz[1]) == np.ndarray:
-        print("oops")
